77b94d34e734993b79f1e20e9ef8e45cfd0da9be
timhagn <mail@timhagn.com>
2015-11-06 21:52:55 +0100

    Thnx to https://github.com/treehouse on step further on : )

--- android/app/src/main/java/com/timhagn/rngloc/RNGLocationModule.java
+++ android/app/src/main/java/com/timhagn/rngloc/RNGLocationModule.java
@@ -10,6 +10,7 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.renderscript.ScriptGroup;
+import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
@@ -23,76 +24,46 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Map;
 
 /**
  * Created by hagn on 11/5/15.
  */
-public class RNGLocationModule extends ReactContextBaseJavaModule  {
+public class RNGLocationModule extends ReactContextBaseJavaModule implements LocationProvider.LocationCallback {
 
-    public static final String REACT_CLASS = "RNGLoc";
-    private ReactApplicationContext mReactContext;
+    public static final String REACT_CLASS = "RNGLocation";
+    public static final String TAG = RNGLocationModule.class.getSimpleName();
+
+    ReactApplicationContext mReactContext;
     private Location mLastLocation;
-    private Boolean mIsBound;
+    private LocationProvider mLocationProvider;
 
     public RNGLocationModule(ReactApplicationContext reactContext) {
         super(reactContext);
         mReactContext = reactContext;
-    }
-    private GPLocationService mBoundService;
-
-    private ServiceConnection mConnection = new ServiceConnection() {
-        public void onServiceConnected(ComponentName className, IBinder service) {
-            // This is called when the connection with the service has been
-            // established, giving us the service object we can use to
-            // interact with the service.  Because we have bound to a explicit
-            // service that we know is running in our own process, we can
-            // cast its IBinder to a concrete class and directly access it.
-            mBoundService = ((GPLocationService.LocalBinder)service).getService();
-            mLastLocation = mBoundService.getLocation();
-        }
-
-        public void onServiceDisconnected(ComponentName className) {
-            // This is called when the connection with the service has been
-            // unexpectedly disconnected -- that is, its process crashed.
-            // Because it is running in our same process, we should never
-            // see this happen.
-            mBoundService = null;
-        }
-    };
-
-    public void doBindService() {
-        // Establish a connection with the service.  We use an explicit
-        // class name because we want a specific service implementation that
-        // we know will be running in our own process (and thus won't be
-        // supporting component replacement by other applications).
-        mReactContext.bindService(new Intent(mBoundService,
-                GPLocationService.class), mConnection, Context.BIND_AUTO_CREATE);
-        mIsBound = true;
-
+        mLocationProvider = new LocationProvider(mReactContext, this);
     }
 
-    public void doUnbindService() {
-        if (mIsBound) {
-            // Detach our existing connection.
-            mReactContext.unbindService(mConnection);
-            mIsBound = false;
-        }
-    }
 
     @Override
     public String getName() {
         return REACT_CLASS;
     }
 
-    public void connectGPLoc() {
-        Intent intent = new Intent();
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        //Check that an app exists to receive the intent
-        if (intent.resolveActivity(mReactContext.getPackageManager()) != null) {
-            mReactContext.startActivity(intent);
-        }
+    protected void onResume() {
+        mLocationProvider.connect();
+    }
+
+    protected void onPause() {
+        mLocationProvider.disconnect();
+    }
+
+    public void handleNewLocation(Location location) {
+        mLastLocation = location;
     }
 
     @ReactMethod
@@ -108,19 +79,8 @@ public class RNGLocationModule extends ReactContextBaseJavaModule  {
                 successCallback.invoke(Longitude, Latitude);
             } catch (Exception e) {
                 e.printStackTrace();
-                sendError("getLocation Error", "location_error");
+                Log.i(TAG, "Location services connected.");
             }
         }
     }
-
-    private void sendError (String message, String type) {
-        WritableMap error = Arguments.createMap();
-        error.putString("message", message);
-        error.putString("type", type);
-
-        mReactContext
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit("locationError", error);
-    }
-
 }
